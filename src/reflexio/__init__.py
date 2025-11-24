cdf2f02ffd85642fd66ce41a8cedbd68315ad90d
aponysus <me@joshuasorrell.com>
2025-11-23 18:23:36 -0600

    Add context manager and update usage docs.

--- src/reflexio/__init__.py
+++ src/reflexio/__init__.py
@@ -2,6 +2,7 @@ from .classify import default_classifier
 from .config import RetryConfig
 from .policy import AsyncRetryPolicy, RetryPolicy, retry
 from .strategies import decorrelated_jitter, equal_jitter, token_backoff
+from .extras import http_classifier, sqlstate_classifier
 
 __all__ = [
     "AsyncRetryPolicy",
@@ -11,5 +12,7 @@ __all__ = [
     "equal_jitter",
     "token_backoff",
     "default_classifier",
+    "http_classifier",
+    "sqlstate_classifier",
     "retry",
 ]
